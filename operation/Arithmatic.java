ddd6919a64d5ba9a61f00070b408caa333cbe65a
Vaishnavizambare16 <vaishuzambare1@gmail.com>
2026-02-27 13:29:56 +0530

    Add files via upload

---
+++ operation/Arithmatic.java
@@ -0,0 +1,18 @@
+package com.operation;
+
+public class Arithmatic {
+	public static void main(String[] args) {
+		int a = 40;
+		int b = 20;
+
+		int c = a + b;
+
+		System.out.println("Addition of " + a + " + " + b + " = " + c);
+		System.out.println("Sub of " + a + " - " + b + " = " + (a - b));
+		System.out.println("Mul of " + a + " * " + b + " = " + (a * b));
+		System.out.println("Div of " + a + " / " + b + " = " + (a / b));
+		System.out.println("Mod of " + a + " % " + b + " = " + (a % b));
+
+	}
+
+}
